fb9bc514d96f5feee1e302fa9e280d8df33537ee
Josh Goldberg <josh@3io.com>
2026-08-02 16:01:14 -0700

    update labels for form elements

--- lab6/quoteAdmin/public/js/quoteForm.js
+++ lab6/quoteAdmin/public/js/quoteForm.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector("select[name='category']").addEventListener('change', (event) => {
     if (event.target.value == "") {
-      document.querySelector("input[name='new_category']").removeAttribute('disabled');
+      document.querySelector("input[name='new_category']").classList.remove('d-none');
+      document.querySelector("#hideNewCategory").classList.add('d-none');
     } else {
-      document.querySelector("input[name='new_category']").setAttribute('disabled',true);
+      document.querySelector("input[name='new_category']").classList.add('d-none');
+      document.querySelector("#hideNewCategory").classList.remove('d-none');
     }
   });
 
@@ -11,5 +13,10 @@ document.addEventListener('DOMContentLoaded', () => {
     if (event.target.value != "") {
       document.querySelector("select[name='category']").selectedIndex = 0;
     }
+  });
+  document.querySelector("#hideNewCategory").addEventListener('click', (event) => {
+    event.target.classList.add('d-none');
+    document.querySelector("input[name='new_category']").classList.remove('d-none');
+    document.querySelector("select[name='category']").selectedIndex = 0;
   });
 });
